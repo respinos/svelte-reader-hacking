905559f13cc11b0d9b616c69d0d0c26be2578c4e
Roger Espinosa <roger@umich.edu>
2023-04-02 13:09:26 -0400

    some changes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import './hacking01.css'
 
 // import Queue from "queue-promise";
-import App from './components/View05.svelte'
+// import App from './components/View05.svelte'
+import App from './components/MessagingRoot.svelte';
 
 const app = new App({
   target: document.getElementById('app'),
