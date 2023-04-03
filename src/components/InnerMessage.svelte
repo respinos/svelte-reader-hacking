9e36115738c1bd37e2c60a9dd1d27ce1882ce49c
Roger Espinosa <roger@umich.edu>
2023-04-02 20:57:36 -0400

    various message experiments

---
+++ src/components/InnerMessage.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { getContext } from 'svelte';
+
+  console.log("INNER CONTEXT", getContext('context'));
+
+  // const { root } = getContext('context');
+  // const emitter = { emit: function() {} };
+
+  const { emitter } = getContext('context');
+
+  let count = 0;
+  function notify() {
+    count += 1;
+    emitter.emit('update.count', count);
+  }
+
+</script>
+
+<pre>AHOY</pre>
+<!-- <pre>MESSAGE: {root.message()}</pre> -->
+<p>
+  <button on:click={notify}>Notify Root</button>
+</p>
